b7f48a2915e661a348466c9be7c6d49ad87b4717
mouradaze <mouradaze10@gmail.com>
2023-11-11 21:28:47 +0100

    Ajout du bean cdi des comptes et les templates

---
+++ src/main/java/ma/aouzen/tpbanqueaouzen/jsf/ListeComptes.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package ma.aouzen.tpbanqueaouzen.jsf;
+
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import java.io.Serializable;
+import java.util.List;
+import ma.aouzen.tpbanqueaouzen.entity.CompteBancaire;
+import ma.aouzen.tpbanqueaouzen.service.GestionnaireCompte;
+
+/**
+ *
+ * @author PC
+ */
+@Named(value = "listeComptes")
+@ViewScoped
+public class ListeComptes implements Serializable {
+    @Inject
+    private GestionnaireCompte gc;
+
+    /**
+     * Creates a new instance of ListeComptes
+     */
+    public ListeComptes() {
+    }
+    
+    public List<CompteBancaire> getAllComptes(){
+        
+        return gc.getAllComptes();
+    }
+    
+}
